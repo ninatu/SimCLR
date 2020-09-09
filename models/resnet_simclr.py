cd7526cdf52b88634d6e55be921ad7ea210dc026
Nina Tuluptceva <nina.tuluptceva@philips.com>
2020-09-09 16:35:36 +0300

    Add support of vgg19

--- models/resnet_simclr.py
+++ models/resnet_simclr.py
@@ -1,3 +1,4 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.models as models
@@ -8,16 +9,22 @@ class ResNetSimCLR(nn.Module):
     def __init__(self, base_model, out_dim):
         super(ResNetSimCLR, self).__init__()
         self.resnet_dict = {"resnet18": models.resnet18(pretrained=False),
-                            "resnet50": models.resnet50(pretrained=False)}
+                            "resnet50": models.resnet50(pretrained=False),
+                            "vgg19": models.vgg19(pretrained=False)
+                            }
 
-        resnet = self._get_basemodel(base_model)
-        num_ftrs = resnet.fc.in_features
+        model = self._get_basemodel(base_model)
 
-        self.features = nn.Sequential(*list(resnet.children())[:-1])
+        try:
+            num_ftrs = model.fc.in_features
+        except:
+            num_ftrs = model.classifier[0].in_features
+
+        self.features = nn.Sequential(*list(model.children())[:-1])
 
         # projection MLP
-        self.l1 = nn.Linear(num_ftrs, num_ftrs)
-        self.l2 = nn.Linear(num_ftrs, out_dim)
+        self.l1 = nn.Linear(num_ftrs, 2048)
+        self.l2 = nn.Linear(2048, out_dim)
 
     def _get_basemodel(self, model_name):
         try:
@@ -29,7 +36,7 @@ class ResNetSimCLR(nn.Module):
 
     def forward(self, x):
         h = self.features(x)
-        h = h.squeeze()
+        h = torch.flatten(h, 1)
 
         x = self.l1(h)
         x = F.relu(x)
--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from simclr import SimCLR
 import yaml
 from data_aug.dataset_wrapper import DataSetWrapper
+import argparse
 
 
 def main():
-    config = yaml.load(open("config.yaml", "r"), Loader=yaml.FullLoader)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', type=str, help='Path to config')
+    config = parser.parse_args().config
+
+    config = yaml.load(open(config, "r"), Loader=yaml.FullLoader)
     dataset = DataSetWrapper(config['batch_size'], **config['dataset'])
 
     simclr = SimCLR(dataset, config)
